dd8f0d1f6b06b10a0136416114195729d6aa60c2
Aslkhnl <36887233+Aslkhnl@users.noreply.github.com>
2018-02-27 20:12:12 +0600

    Update db_credentials.php

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
     
-    define("DB_SERVER", "localhost");
+    define("DB_SERVER", "174.138.15.211");
     define("DB_USER", "ayauly");
     define("DB_PASS", "1q2w3e4R");
     define("DB_NAME", "almau_curriculum.sql");
